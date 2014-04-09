319bd2958b6a5a13084d13fb4c376bd101a2cfff
William Leese <william.leese@meltwater.com>
2014-04-10 08:39:21 +0900

    make puppet-lint happy

--- manifests/instances.pp
+++ manifests/instances.pp
@@ -1,3 +1,4 @@
+# Provide a way to use ENC / Hiera to create resources
 class es::instances ($instance) {
   $real_instance = hiera_hash(es::instances::instance)
   create_resources('es', $real_instance)
