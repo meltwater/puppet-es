812472545f02b4ae471d145567929f82da077a07
William Leese <william.leese@meltwater.com>
2014-08-27 17:33:32 +0900

    allow setting defaults

--- manifests/instances.pp
+++ manifests/instances.pp
@@ -1,5 +1,10 @@
 # Provide a way to use ENC / Hiera to create resources
-class es::instances ($instance) {
+class es::instances ($instance, $defaults = undef) {
   $real_instance = hiera_hash(es::instances::instance)
-  create_resources('es', $real_instance)
+
+  if $defaults {
+    create_resources('es', $real_instance, $defaults)
+  } else {
+    create_resources('es', $real_instance)
+  }
 }
